6bc12657bcd9a3f274d2891018051e3b36e61afe
William Bakst <william@mirascope.io>
2025-03-10 23:26:49 -0700

    feat: basic run functions for making it easier to run MCP servers and connect to them

--- python/mcp_community/__init__.py
+++ python/mcp_community/__init__.py
@@ -1,10 +1,20 @@
 """Model Context Protocol Community."""
 
 import importlib.metadata
+from contextlib import suppress
 
-from .simple_mcp import SimpleMCP
+from .run import mcp_client, run_mcp, run_mcp_async
+
+with suppress(ImportError):
+    from . import servers as servers
 
 __version__ = importlib.metadata.version("mcp-community")
 
 
-__all__ = ["SimpleMCP"]
+__all__ = [
+    "__version__",
+    "mcp_client",
+    "run_mcp",
+    "run_mcp_async",
+    "servers",
+]
