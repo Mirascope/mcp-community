cc96bbdfc0da31349e20d4458bff1f39820c042e
Koudai Aono <koudai@mirascope.io>
2025-03-13 17:20:58 +0900

    fix: update __init__.py

--- python/mcp_community/servers/__init__.py
+++ python/mcp_community/servers/__init__.py
@@ -3,9 +3,10 @@
 from contextlib import suppress
 
 from .calculator import CalculatorMCP
+from .filesystem import FileSystemMCP
 
 with suppress(ImportError):
     from .duckduckgo import DuckDuckGoMCP as DuckDuckGoMCP
 
 
-__all__ = ["CalculatorMCP", "DuckDuckGoMCP"]
+__all__ = ["CalculatorMCP", "DuckDuckGoMCP", "FileSystemMCP"]
