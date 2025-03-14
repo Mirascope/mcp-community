194d68d2f4a987a5b68fc8de13f413ac13d568c7
Koudai Aono <koudai@mirascope.io>
2025-03-14 17:38:47 +0900

    feat: add slack server

--- python/mcp_community/servers/__init__.py
+++ python/mcp_community/servers/__init__.py
@@ -6,6 +6,7 @@ from .calculator import CalculatorMCP
 
 with suppress(ImportError):
     from .duckduckgo import DuckDuckGoMCP as DuckDuckGoMCP
+    from .slack import SlackMCP as SlackMCP
 
 
-__all__ = ["CalculatorMCP", "DuckDuckGoMCP"]
+__all__ = ["CalculatorMCP", "DuckDuckGoMCP", "SlackMCP"]
